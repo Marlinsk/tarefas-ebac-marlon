6c1ff0fe5813708ebf7c5e812f343cf83d7b8b7a
Marlinsk <Marlonnlmx@outlook.com>
2025-08-13 01:43:40 -0300

    Exercício do modulo 9

---
+++ mod09/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 20.17;
+        System.out.println(num1);
+
+        Double num2 = num1;
+        System.out.print(num2);
+    }
+}
