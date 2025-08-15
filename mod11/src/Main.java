09d5836455da3cffba91b8508af9dde6cba020b5
Marlinsk <Marlonnlmx@outlook.com>
2025-08-14 23:32:20 -0300

    Correção de exercício

--- mod11/src/Main.java
+++ mod11/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,21 +9,5 @@ public class Main {
 
         System.out.println("\nNomes em ordem alfabética:");
         list1.forEach(System.out::println);
-
-        Map<String, List<String>> genderGroup = new LinkedHashMap<>();
-        genderGroup.put("Feminino", new ArrayList<>());
-        genderGroup.put("Masculino", new ArrayList<>());
-
-        String input2 = "Sophia Maria Cecília Maximo - Feminino, Martin Pedro Javier - Masculino, Eleanor Lamb - Feminino, Danilo Raul Heitor Mendes - Masculino, Mariane Marli Lindespoon - Feminino, Raul Rodrigo Caldeira - Masculino, Elaine Antonella Cláudia Araújo - Feminino, Maitê Florença Carvalho - Feminino";
-        Map<String, List<String>> list2 = Arrays.stream(input2.split("\\s*,\\s*")).map(p -> p.split("\\s*-\\s*")).filter(parts -> parts.length == 2).collect(Collectors.groupingBy(parts -> parts[1].trim(), () -> genderGroup, Collectors.mapping(parts -> parts[0].trim(), Collectors.toList())));
-        list2.values().forEach(Collections::sort);
-
-        // Feminino
-        System.out.println("\nMulheres:");
-        list2.getOrDefault("Feminino", Collections.emptyList()).forEach(System.out::println);
-
-        // Masculino
-        System.out.println("\nHomens:");
-        list2.getOrDefault("Masculino", Collections.emptyList()).forEach(System.out::println);
     }
 }
