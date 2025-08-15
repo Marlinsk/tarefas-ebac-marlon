9d5ae0a1d0c26df6eddaeadcb5b282e73ce7f5be
Marlinsk <Marlonnlmx@outlook.com>
2025-08-14 23:42:02 -0300

    fix: Scanner adicionado + remoção da variável input1

--- mod11/src/Main.java
+++ mod11/src/Main.java
@@ -1,10 +1,16 @@
 import java.util.*;
 
+// Lists of names to copy to input:
+// Sophia Maria Cecília Maximo, Martin Pedro Javier, Eleanor Lamb, Danilo Raul Heitor Mendes, Mariane Marli Lindespoon, Raul Rodrigo Caldeira, Elaine Antonella Cláudia Araújo, Maitê Florença Carvalho
+
 public class Main {
     public static void main(String[] args) {
-        String input1 = "Sophia Maria Cecília Maximo, Martin Pedro Javier, Eleanor Lamb, Danilo Raul Heitor Mendes, Mariane Marli Lindespoon, Raul Rodrigo Caldeira, Elaine Antonella Cláudia Araújo, Maitê Florença Carvalho";
+        Scanner scanner = new Scanner(System.in);
 
-        List<String> list1 = new LinkedList<>(Arrays.asList(input1.split("\\s*,\\s*")));
+        System.out.println("Digite uma lista de nomes separados por vírgula e depois digite enter: ");
+        String input  = scanner.nextLine();
+
+        List<String> list1 = new LinkedList<>(Arrays.asList(input.split("\\s*,\\s*")));
         Collections.sort(list1);
 
         System.out.println("\nNomes em ordem alfabética:");
